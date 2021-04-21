25af559aca29f449450c1ac0c70df42702717c82
i-cluster <jhlee9091@gmail.com>
2021-04-21 15:01:05 +0900

    [S04P31A205-41] setup : set project component structure

---
+++ frontend/src/views/HallSetting.vue
@@ -0,0 +1,16 @@
+<template>
+  <!-- 메인 홀 & 게임 세팅 -->
+  <div class="hall-setting">
+    <div class="h3"> 홀 & 게임 세팅 </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HallSetting',
+}
+</script>
+
+<style>
+
+</style>
