79664db8c6933fafa85476cd3dcc82d7d902640e
윤서완 <96missha@hanmail.net>
2021-04-22 15:23:41 +0900

    [#S04P31A205-8] feat : add CatchMindController-send answer

---
+++ backend/src/main/java/com/ssafy/icebreaking/model/CatchMindDto.java
@@ -0,0 +1,22 @@
+package com.ssafy.icebreaking.model;
+
+public class CatchMindDto {
+
+	private int quesid;
+	private String answer;
+	
+	public int getQuesid() {
+		return quesid;
+	}
+	public void setQuesid(int quesid) {
+		this.quesid = quesid;
+	}
+	public String getAnswer() {
+		return answer;
+	}
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+	
+	
+}
--- backend/src/main/java/com/ssafy/icebreaking/model/test.java
+++
@@ -1,5 +0,0 @@
-package com.ssafy.icebreaking.model;
-
-public class test {
-
-}
