96ffb47f9dcb863bf552406211b7d746a19cb327
이규용 <gyuyong290@gmail.com>
2021-04-22 10:15:08 +0900

    [#S04P31A205-48] feat: catch mind - painting part done

--- frontend/src/views/room/GamePlay.vue
+++ frontend/src/views/room/GamePlay.vue
@@ -1,15 +1,19 @@
 <template>
   <div id="game-play">
-    <h3> 게임 </h3>
+    <h3>게임</h3>
+    <catch-mind></catch-mind>
   </div>
 </template>
 
 <script>
+import CatchMind from "@/components/CatchMind.vue";
+
 export default {
- name: 'GamePlay',
-}
+  name: "GamePlay",
+  components: {
+    CatchMind,
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
