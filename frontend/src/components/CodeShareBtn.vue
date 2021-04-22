7441acb8b465d0f0c765cf189ab1eb2837856344
JiHee_Hong <vegavenus0920@gmail.com>
2021-04-22 12:30:59 +0900

    [#S04P31A205-36] feat : add CodeShareBtn component

---
+++ frontend/src/components/CodeShareBtn.vue
@@ -0,0 +1,25 @@
+<template>
+                    <img
+        class="kakao_btn"
+        src="https://developers.kakao.com/assets/img/about/logos/kakaolink/kakaolink_btn_medium.png"
+        @click="clickKakaoShare"
+    />
+</template>
+<script type="text/Javascript" src="https://developers.kakao.com/sdk/js/kakao.min.js"></script>
+<script>
+export default {
+  name: 'CodeShareBtn',
+methods: {
+     clickKakaoShare() {
+        Kakao.Link.sendCustom({
+            templateId: 52239,
+        });
+    },
+},
+
+}
+</script>
+
+<style>
+
+</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,6 +5,10 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+// Kakao init
+window.Kakao.init('fa32f0c8aa0aada5f907c55aa1826140');
+window.Kakao.isInitialized();
+
 new Vue({
   store,
   router,
