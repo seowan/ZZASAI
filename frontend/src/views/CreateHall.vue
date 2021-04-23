8c0bbafb5785be5ae415d9e178862e04c536e05d
이송영 <bulgen@naver.com>
2021-04-23 16:46:50 +0900

    [#] Refac: add Vue.Draggable

--- frontend/src/views/CreateHall.vue
+++ frontend/src/views/CreateHall.vue
@@ -4,38 +4,37 @@
     <div class="h3"> 홀 & 게임 세팅 </div>
 
     <!-- OrderBlock -->
-  <div class="info">
-    <div id="title">PUZZLE</div>
-    <div id="rest">Drag and drop the colored pieces on the puzzle below. (better images are on their way) </div>
-    <div id="rest2">You can put multiple pieces in the same area but you can't interchange them, only the last one you put will be accessible.</div>
-  </div>
-  
-  <div id="images">
-    <div id="drag4" draggable="true" @ondragstart="drag(event)"></div>
-    <div id="drag5" draggable="true" @ondragstart="drag(event)"></div>
-    <div id="drag6" draggable="true" @ondragstart="drag(event)"></div>
-    <div id="drag7" draggable="true" @ondragstart="drag(event)"></div>
-    <div id="drag8" draggable="true" @ondragstart="drag(event)"></div>
-    <div id="drag9" draggable="true" @ondragstart="drag(event)"></div>
-    <div id="drag10" draggable="true" @ondragstart="drag(event)"></div>
-    <div id="drag11" draggable="true" @ondragstart="drag(event)"></div>
-    <div id="drag12" draggable="true" @ondragstart="drag(event)"></div>
-  </div>
-  
-  <div id="text">
-    <div id="div1" @ondrop="drop(event)" @ondragover="allowDrop(event)"></div>
-    <div id="div2" @ondrop="drop(event)" @ondragover="allowDrop(event)"></div>
-    <div id="div3" @ondrop="drop(event)" @ondragover="allowDrop(event)"></div>
-    <div id="div4" @ondrop="drop(event)" @ondragover="allowDrop(event)"></div>
-    <div id="div5" @ondrop="drop(event)" @ondragover="allowDrop(event)"></div>
-    <div id="div6" @ondrop="drop(event)" @ondragover="allowDrop(event)"></div>
-    <div id="div7" @ondrop="drop(event)" @ondragover="allowDrop(event)"></div>
-    <div id="div8" @ondrop="drop(event)" @ondragover="allowDrop(event)"></div>
-    <div id="div9" @ondrop="drop(event)" @ondragover="allowDrop(event)"></div>
-  </div>
-  
-  <div class="info"><p>Congratulations if you finished!</p></div>
+    <div class="row">
+      <div class="col-3">
+        <h3>Draggable 1</h3>
+        <draggable class="list-group" :list="list1" group="people" @change="log">
+          <div
+            class="list-group-item"
+            v-for="(element, index) in list1"
+            :key="element.name"
+          >
+            {{ element.name }} {{ index }}
+          </div>
+        </draggable>
+      </div>
 
+      <div class="col-3">
+        <h3>Draggable 2</h3>
+        <draggable class="list-group" :list="list2" group="people" @change="log">
+          <div
+            class="list-group-item"
+            v-for="(element, index) in list2"
+            :key="element.name"
+          >
+            {{ element.name }} {{ index }}
+          </div>
+        </draggable>
+      </div>
+
+      <rawDisplayer class="col-3" :value="list1" title="List 1" />
+
+      <rawDisplayer class="col-3" :value="list2" title="List 2" />
+    </div>
 
     <!-- HelpIcon -->
     <HelpIcon />
@@ -45,28 +44,46 @@
 </template>
 
 <script>
+// Vue.Draggable https://github.com/SortableJS/Vue.Draggable
+import draggable from 'vuedraggable'
 import HelpIcon from '@/components/HelpIcon'
 
 export default {
   name: 'CreateHall',
   components: {
+    draggable,
     HelpIcon,
   },
+  data() {
+    return {
+      list1: [
+        { name: "John", id: 1 },
+        { name: "Joao", id: 2 },
+        { name: "Jean", id: 3 },
+        { name: "Gerard", id: 4 }
+      ],
+      list2: [
+        { name: "Juan", id: 5 },
+        { name: "Edgard", id: 6 },
+        { name: "Johnson", id: 7 }
+      ]
+    };
+  },
   methods: {
-    allowDrop: function (ev) {
-        ev.preventDefault();
+    add: function() {
+      this.list.push({ name: "Juan" });
     },
-
-    drag: function (ev) {
-        ev.dataTransfer.setData("text", ev.target.id);
+    replace: function() {
+      this.list = [{ name: "Edgard" }];
     },
-
-    drop: function (ev) {
-        ev.preventDefault();
-        var data = ev.dataTransfer.getData("text");
-        ev.target.appendChild(document.getElementById(data));
+    clone: function(el) {
+      return {
+        name: el.name + " cloned"
+      };
     },
-
+    log: function(evt) {
+      window.console.log(evt);
+    }
   },
 }
 </script>
@@ -84,100 +101,5 @@ body {
 .create-hall {
   margin: 0;
   height: 100%;
-}
-
-
-.info {
-  position:relative;
-  font-family:Arial;
-  margin:10px;
-  text-align:center;
-  border-radius:5px;
-  font-size:15px;
-}
-
-#title {
-  font-size:20px;
-}
-
-#rest2 {
-  font-size:13px;
-}
-
-#text div {
-  position:relative;
-  height: 200px;
-  width: 200px;
-  border: 1px dashed gray;
-  display: inline-block;
-  margin: 0;
-}
-
-#text {
-  width:614px;
-  height:614px;
-  background:black;
-  border:5px solid black;
-  margin:auto;
-}
-
-#images {
-  width: 100%
-}
-
-img {
-  height: 200px;
-  width: 200px;
-}
-
-#drag4,
-#drag5,
-#drag6,
-#drag7,
-#drag8,
-#drag9,
-#drag10,
-#drag11,
-#drag12 {
-  height: 200px;
-  width: 200px;
-  
-  display: inline-block;
-}
-
-#drag4 {
-  background: linear-gradient(to bottom right, orange, orange, yellow);
-}
-
-#drag5 {
-  background: linear-gradient(to right, red, orange);
-}
-
-#drag6 {
-  background: linear-gradient(red, orange);
-}
-
-#drag7 {
-  background: linear-gradient(to bottom right, red, red, orange);
-}
-
-#drag8 {
-  background:yellow;
-}
-
-#drag9 {
-  background: linear-gradient(orange, yellow);
-}
-
-#drag10 {
-  background: linear-gradient(to right, orange, yellow);
-}
-
-#drag11 {
-  background: linear-gradient(red, orange);
-}
-
-#drag12 {
-  background: linear-gradient(to right, red,orange);
 }
 </style>
