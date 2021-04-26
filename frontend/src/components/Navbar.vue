5fa9b4f440c3a984bc6f6107f103096133673b48
윤서완 <96missha@hanmail.net>
2021-04-26 19:01:52 +0900

    [#S04P31A205-65] feat : edit detail

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="navbar">
     <div id="logo">무슨 서비스</div>
-    <div class="serve-intro">서비스 소개</div>
+    <div class="row">
+      <div class="align-center mt-60">
+        <h3 class="point" style="font-size:22px; color:red; display:inline-block;">짜</h3>
+        <h3 class="about" style="font-size:22px; display:inline-block;">식, 이제 우리 친구</h3>
+        <h3 class="point" style="font-size:22px; color:red; display:inline-block;">사이</h3>
+        <h3 class="about" style="font-size:22px; display:inline-block;">다</h3>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -12,5 +19,7 @@ export default {
 </script>
 
 <style>
-
+.row{
+  padding-right: 15px;
+}
 </style>
