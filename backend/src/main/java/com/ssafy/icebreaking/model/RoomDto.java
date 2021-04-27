195a2f9bb33cf7a224faa8fcde42790b6f08101b
윤서완 <96missha@hanmail.net>
2021-04-27 17:29:33 +0900

    [#S04P31A205-83] feat : add RoomController_insert when room is created

---
+++ backend/src/main/java/com/ssafy/icebreaking/model/RoomDto.java
@@ -0,0 +1,50 @@
+package com.ssafy.icebreaking.model;
+
+public class RoomDto {
+	private String roomCode;
+	private String roomAdmin;
+	private String Game1;
+	private String Game2;
+	private String Game3;
+
+	public String getRoomCode() {
+		return roomCode;
+	}
+
+	public void setRoomCode(String roomCode) {
+		this.roomCode = roomCode;
+	}
+
+	public String getRoomAdmin() {
+		return roomAdmin;
+	}
+
+	public void setRoomAdmin(String roomAdmin) {
+		this.roomAdmin = roomAdmin;
+	}
+
+	public String getGame1() {
+		return Game1;
+	}
+
+	public void setGame1(String game1) {
+		Game1 = game1;
+	}
+
+	public String getGame2() {
+		return Game2;
+	}
+
+	public void setGame2(String game2) {
+		Game2 = game2;
+	}
+
+	public String getGame3() {
+		return Game3;
+	}
+
+	public void setGame3(String game3) {
+		Game3 = game3;
+	}
+
+}
