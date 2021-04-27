c5c2ef3b51c3489d0097fc809c71f32ac653bbf0
이규용 <gyuyong290@gmail.com>
2021-04-27 17:11:10 +0900

    [#S04P31A205-49 Actions (Type . to access issue actions)  #S04P31A205-75] feat: socket.io chatting with vue.js succeed

--- frontend/src/components/CatchMind.vue
+++ frontend/src/components/CatchMind.vue
@@ -1,8 +1,33 @@
 <template>
   <div>
-    <h1>채팅방</h1>
+    <canvas
+      @mousedown="startPainting"
+      @mouseup="stopPainting"
+      @mousemove="onMouseMove"
+      @mouseleave="stopPainting"
+      id="canvas"
+    ></canvas>
+    <div>
+      <div
+        v-for="color in colors"
+        :key="color"
+        class="colorPicker"
+        :class="color"
+        @click="strokeColorHandler(color)"
+      ></div>
+    </div>
+    <div v-for="i in 3" :key="i" @click="strokeSizeHandler(i)">
+      {{ i }}번째 크기
+    </div>
+    <div class="btn eraser" @click="strokeColorHandler('white')">지우개</div>
+    <div id="btn clearAll" @click="clearAll">
+      모두 지우기
+    </div>
+
     <ul>
-      <li v-for="(msg, index) in messages" :key="index">{{ msg.message }}</li>
+      <li v-for="(msg, index) in messages" :key="index">
+        {{ msg.name }}: {{ msg.text }}
+      </li>
     </ul>
     <div>
       <input type="text" @keyup.enter="sendMessage()" v-model="message" />
@@ -14,25 +39,114 @@
 import io from "socket.io-client";
 
 export default {
-  name: "room",
+  name: "CatchMind",
   data() {
     return {
+      painting: false,
+      canvas: null,
+      ctx: null,
+      canvasHeight: 500,
+      canvasWidth: 500,
+      colors: [
+        "black",
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "blue",
+        "darkviolet",
+      ],
+
+      text: "",
       messages: [],
+      nickname: "user",
       // 1) 서버와 연결
-      socket: io("localhost:3001"),
+      socket: io("localhost:3000"),
     };
   },
-  methods: {
-    sendMessage(message) {
-      // 2) 채팅메세지를 서버로 전송
-      this.socket.emit("SEND_MESSAGE", { message });
-    },
-  },
   mounted() {
+    this.canvas = document.getElementById("canvas");
+    this.ctx = this.canvas.getContext("2d");
+    this.ctx.strokeStyle = "#2c2c2c";
+    this.ctx.lineWidth = 2.5;
+
+    // Resize canvas
+    this.canvas.height = this.canvasHeight;
+    this.canvas.width = this.canvasWidth;
+
     // 3) 서버의 변경사항을 수신
-    this.socket.on("MESSAGE", (data) => {
+    this.socket.on("receive message", (name, text) => {
+      var data = { name: name, text: text };
       this.messages = [...this.messages, data];
     });
   },
+  methods: {
+    onMouseMove(event) {
+      const x = event.offsetX;
+      const y = event.offsetY;
+      if (!this.painting) {
+        this.ctx.beginPath();
+        this.ctx.moveTo(x, y);
+      } else {
+        this.ctx.lineTo(x, y);
+        this.ctx.stroke();
+      }
+    },
+    startPainting() {
+      this.painting = true;
+    },
+    stopPainting() {
+      this.painting = false;
+      // this.ctx.beginPath();
+    },
+    strokeSizeHandler(size) {
+      this.ctx.lineWidth = 1.0 + 5 * (size - 1); //1: 1, 2: 6, 3: 11
+    },
+    strokeColorHandler(color) {
+      this.ctx.strokeStyle = color;
+    },
+    clearAll() {
+      this.ctx.fillStyle = "white";
+      this.ctx.fillRect(0, 0, this.canvasWidth, this.canvasHeight);
+    },
+    sendMessage() {
+      // 2) 채팅메세지를 서버로 전송
+      this.socket.emit("send message", this.nickname, this.text); //"gyu" 부분만 유저이름으로 바꾸면 됨
+    },
+  },
 };
 </script>
+
+<style>
+.colorPicker {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+}
+#canvas {
+  border: 3px solid black;
+  height: 500px;
+  width: 500px;
+}
+.black {
+  background-color: black;
+}
+.red {
+  background-color: red;
+}
+.orange {
+  background-color: orange;
+}
+.yellow {
+  background-color: yellow;
+}
+.green {
+  background-color: green;
+}
+.blue {
+  background-color: blue;
+}
+.darkviolet {
+  background-color: darkviolet;
+}
+</style>
--- socket_test/server.js
+++ socket_test/server.js
@@ -1,26 +1,35 @@
-// app.js
-const cors = require("cors");
-const express = require("express");
+// server.js
 
-const app = express();
+var express = require("express");
+var app = express();
+var http = require("http").Server(app); //1
+var io = require("socket.io")(http, { cors: { origin: "*" } }); //1. **allow all cors**
+// app.get("/", function (req, res) {
+//   //2
+//   res.sendFile(__dirname + "/client.html");
+// });
 
-// CORS 설정
-app.use(cors());
-
-app.get("/", function (req, res) {
-  // '/' 위치에 'get'요청을 받는 경우,
-  res.send("Hello World!"); // "Hello World!"를 보냅니다.
-});
-
-const server = app.listen(3001, function () {
-  console.log("server running on port 3001");
-});
-
-const io = require("socket.io")(server);
-
+var count = 1;
 io.on("connection", function (socket) {
-  console.log("connected~");
-  socket.on("SEND_MESSAGE", function (data) {
-    io.emit("MESSAGE", data);
+  //3. interact with clients
+  console.log("user connected: ", socket.id); //3-1. client connected
+  var name = "user" + count++; //3-1. temporal username
+  io.to(socket.id).emit("change name", name); //3-1
+
+  socket.on("disconnect", function () {
+    //3-2. client disconnected
+    console.log("user disconnected: ", socket.id);
   });
+
+  socket.on("send message", function (name, text) {
+    //3-3. chat service
+    var msg = name + " : " + text;
+    console.log(msg);
+    io.emit("receive message", name, text);
+  });
+});
+
+//4
+http.listen(3000, function () {
+  console.log("server on!");
 });
