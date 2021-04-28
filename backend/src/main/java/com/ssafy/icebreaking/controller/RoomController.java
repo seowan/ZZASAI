23e8947fe9235f2a95749ce53e04c5ca6857ccc3
윤서완 <96missha@hanmail.net>
2021-04-28 16:11:05 +0900

    [#S04P31A205-107] add url

--- backend/src/main/java/com/ssafy/icebreaking/controller/RoomController.java
+++ backend/src/main/java/com/ssafy/icebreaking/controller/RoomController.java
@@ -1,14 +1,9 @@
 package com.ssafy.icebreaking.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
-
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ssafy.icebreaking.model.RoomDto;
 import com.ssafy.icebreaking.model.service.RoomService;
 
+@CrossOrigin(origins= {"http://zzasai.com"})
 @RestController
 @RequestMapping("/room")
 public class RoomController {
