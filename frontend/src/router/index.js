e5db383aa3844a92c0c785d458a3601222b7ef25
jsman <jsman9777@gmail.com>
2021-04-29 09:41:08 +0900

    Conflict resolution

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -9,7 +9,12 @@ import CharTest from '../views/room/CharTest.vue'
 import HostName from '../views/room_register/HostName.vue'
 import RoomName from '../views/room_register/RoomName.vue'
 import UserName from '../views/room_register/UserName.vue'
-
+import CharTest2 from '../views/room/CharTest2.vue'
+import CharTest3 from '../views/room/CharTest3.vue'
+import CharTest4 from '../views/room/CharTest4.vue'
+import CharTest5 from '../views/room/CharTest5.vue'
+import CharTest6 from '../views/room/CharTest6.vue'
+import CharTest7 from '../views/room/CharTest7.vue'
 Vue.use(VueRouter)
 
 const routes = [
@@ -58,8 +63,38 @@ const routes = [
     name: 'UserName',
     component: UserName
   },
+  {
+    path: '/char-test2',
+    name: 'CharTest2',
+    component: CharTest2
+  },
+  {
+    path: '/char-test3',
+    name: 'CharTest3',
+    component: CharTest3
+  },
+  {
+    path: '/char-test4',
+    name: 'CharTest4',
+    component: CharTest4
+  },
+  {
+    path: '/char-test5',
+    name: 'CharTest5',
+    component: CharTest5
+  },
+  {
+    path: '/char-test6',
+    name: 'CharTest6',
+    component: CharTest6
+  },
+  {
+    path: '/char-test7',
+    name: 'CharTest7',
+    component: CharTest7
+  },
 ]
-
+ 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
---
+++ frontend/src/views/room/CharTest3.vue
@@ -0,0 +1,36 @@
+<template>
+  <!-- 캐릭터 테스트 -->
+  <div>
+    <h1> 캐릭터(성향) 테스트 </h1>
+    <div class="py-3 my-2"></div>
+    <h1>2/4</h1>
+    <br><br>
+    <h1>데스노트를 보고 '만약 나에게 데스노트가 있다면'을 상상해본적이 있는가?</h1>
+    <br>
+    <b-button type="button" variant="primary" class="btn btn-lg" v-on:click="next">그렇다</b-button>
+    <b-button type="button" variant="danger" class="btn btn-lg" v-on:click="next">아니다</b-button>
+
+  </div>
+  
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+        
+      }
+  },
+  methods: {
+    next() {
+      this.$router.push('/char-test4');
+    }
+  }
+}
+
+</script>
+
+<style>
+
+</style>
