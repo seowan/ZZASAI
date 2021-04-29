5cbf0186bb69b83cf2fc05fb25583abe2e058122
i-cluster <jhlee9091@gmail.com>
2021-04-29 16:16:07 +0900

    [#S04P31A205-90] feat : Setting username

--- frontend/src/views/Main.vue
+++ frontend/src/views/Main.vue
@@ -2,13 +2,19 @@
   <!-- 메인 홈 -->
   <div id="main">
     <h3> 메인 </h3>
-    <router-link to="/test"> 입장하기 </router-link>
+    <input type="text" v-model="userName">
+    <router-link :to="{ name: 'Test', params: { userName: userName }}"> 입장하기 </router-link>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Main',
+  data: function () {
+    return {
+      userName: ''
+    }
+  }
 }
 </script>
 
--- frontend/src/views/Test.vue
+++ frontend/src/views/Test.vue
@@ -1,22 +1,28 @@
 <template>
   <div id="test">
-    <user-video :stream-manager="publisher" />
-    <user-video v-for="sub in subscribers" :key="sub.stream.connection.connectionId" :stream-manager="sub" />
+    <!-- 내 비디오 -->
+    <!-- <user-video class="my-video" :stream-manager="publisher" /> -->
+    <!-- 전체 비디오 -->
+    <user-video class="user-videos" v-for="sub in subscribers" :key="sub.stream.connection.connectionId" :stream-manager="sub" />
+    <div class="leave-btn" @click="leaveSession">나가기</div>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
 import { OpenVidu } from 'openvidu-browser'
+import UserVideo from '@/components/UserVideo'
 
 axios.defaults.headers.post['Content-Type'] = 'application/json'
 
-// const OPENVIDU_SERVER_URL = "https://" + location.hostname + ":4443"
-const OPENVIDU_SERVER_URL = "https://192.168.99.100:4443"
+const OPENVIDU_SERVER_URL = "https://" + location.hostname + ":4443"
 const OPENVIDU_SERVER_SECRET = "MY_SECRET"
 
 export default {
   name: 'Test',
+  components: {
+    UserVideo
+  },
   data: function () {
     return {
       OV: undefined,
@@ -25,7 +31,6 @@ export default {
       subscribers: [],
 
       mySessionId: 'SessionA',
-      myUserName: 'Participant' + Math.floor(Math.random() * 100),
 
       mainStreamManager: undefined,
     }
@@ -66,17 +71,28 @@ export default {
 					.then(data => resolve(data.id))
 					.catch(error => {
 						if (error.response.status === 409) {
-							resolve(sessionId);
+							resolve(sessionId)
 						} else {
 							console.warn(`No connection to OpenVidu Server. This may be a certificate error at ${OPENVIDU_SERVER_URL}`);
 							if (window.confirm(`No connection to OpenVidu Server. This may be a certificate error at ${OPENVIDU_SERVER_URL}\n\nClick OK to navigate and accept it. If no certificate warning is shown, then check that your OpenVidu Server is up and running at "${OPENVIDU_SERVER_URL}"`)) {
-								location.assign(`${OPENVIDU_SERVER_URL}/accept-certificate`);
+								location.assign(`${OPENVIDU_SERVER_URL}/accept-certificate`)
 							}
-							reject(error.response);
+							reject(error.response)
 						}
-					});
-			});
+					})
+			})
 		},
+    leaveSession: function () {
+      if (this.session) this.session.disconnect()
+
+			this.session = undefined
+			this.mainStreamManager = undefined
+			this.publisher = undefined
+			this.subscribers = []
+			this.OV = undefined
+
+      this.$router.push({ name: 'Main' })
+    }
   },
   mounted() {
     // openvidu 객체 생성
@@ -85,21 +101,29 @@ export default {
     // 세션 초기화
     this.session = this.OV.initSession()
 
-    // 새로운 stream(유저) 추가
     this.session.on('streamCreated', ({ stream }) => {
       let subscriber = this.session.subscribe(stream)
       this.subscribers.push(subscriber)
     })
 
+    this.session.on('streamDestroyed', ({ stream }) => {
+      const index = this.subscribers.indexOf(stream.streamManager, 0)
+      if (index >= 0) {
+        this.subscribers.splice(index, 1)
+      }
+    })
+
+    // Openvidu 서버(Docker)에서 토큰 받기 -> 세션에 연결
     this.getToken(this.mySessionId).then(token => {
-      this.session.connect(token, { clientData: this.myUserName })
+      // 내이름을 params로 전달
+      this.session.connect(token, { clientData: this.$route.params.userName })
         .then(() => {
           let publisher = this.OV.initPublisher(undefined, {
             audioSource: undefined,
             videoSource: undefined,
-            publishAudio: true,
-            publishVideo: true,
-            resolution: '640x480',
+            publishAudio: true,     // 오디오 설정
+            publishVideo: true,     // 비디오 설정
+            resolution: '320x240',  // 해상도
             frameRate: 30,
             insertMode: 'APPEND',
             mirror: false
@@ -107,6 +131,9 @@ export default {
 
           this.publisher = publisher
           this.mainStreamManager = publisher
+
+          // 유저 subscriber에 나도 추가
+          this.subscribers.push(publisher)
   
           this.session.publish(this.publisher)
         })
