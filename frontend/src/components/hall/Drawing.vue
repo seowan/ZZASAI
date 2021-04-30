168ff34d14b2a27584b406359fb539ae950620d9
JiHee_Hong <vegavenus0920@gmail.com>
2021-04-30 12:11:42 +0900

    [#] Refac : Add mbti component name and connect team divide modal

--- frontend/src/components/hall/Drawing.vue
+++ frontend/src/components/hall/Drawing.vue
@@ -2,21 +2,35 @@
   <div class="col-2 hall-card">
     <div class="hall-card-inner">
       <div class="hall-card-front">
-        <img src="@/assets/icons/pencil.png" alt="Avatar" style="width:100px;height:100px;">
+        <img
+          src="@/assets/icons/pencil.png"
+          alt="Avatar"
+          style="width:100px;height:100px;"
+        />
       </div>
       <div class="hall-card-back">
         <h2 class="hall-card-text">캐치마인드</h2>
+        <TeamDivideModal
+          v-if="isModalViewed"
+          @close-modal="isModalViewed = false"
+        />
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import TeamDivideModal from "@/components/TeamDivideModal";
+
 export default {
-  name: 'Drawing',
-}
+  name: "Drawing",
+  components: {
+    TeamDivideModal,
+  },
+  data() {
+    return { isModalViewed: true };
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
--- frontend/src/components/mbti/Lizard.vue
+++ frontend/src/components/mbti/Lizard.vue
@@ -5,21 +5,24 @@
     <div class="py-3 my-1"></div>
     <div class="row">
       <div class="col-4 offset-md-2">
-        <img src="~@/assets/mbti/octops.png" alt="mbti image" />
+        <img src="~@/assets/mbti/lizard.png" alt="mbti image" />
       </div>
       <div class="col-5">
-        <h2><br />문어는 지능이 높기로 유명하다.</h2>
-        <br />
         <h2>
-          통발에 갇히더라도 탈출에 성공하고, 간단한 수학 문제를 풀기도 한다.
-          심지어 월드컵 시즌엔 ‘점쟁이 문어’로 불리며 게임의 승패도 맞추기도
-          했다.
+          <br />
+          똑똑하고 환경에 적응을 잘하고, 호기심이 많다는 점에서 도마뱀과 INTP은
+          공통점이 많다.
         </h2>
         <br />
         <h2>
-          문어는 타깃이 생기면 그 누구보다 전략적으로 빨리 접근하기 때문에 사냥
-          성공률도 상당히 높은데 목표가 생기면 그 누구보다 빨리 달려간다는
-          점에서 INTJ와 비슷하다고 볼 수 있다.
+          동물학자들은 도마뱀이 자신만의 방법으로 사냥을 하는데 그 수법이 매우
+          창의적이라고 말한다. 곤충을 잡기 어려운 환경에서 살다 보니 다양한
+          아이디어를 개발하게 되었다는 게 그들의 말이다.
+        </h2>
+        <br />
+        <h2>
+          기발하고 혁신적이며 엉뚱한 아이디어를 많이 내는 INTP와 비교되는 이유도
+          이 때문이다.
         </h2>
         <br />
         <button type="button" class="btn btn-dark btn-lg" v-on:click="next">
@@ -34,7 +37,7 @@
 
 <script>
 export default {
-  name: "Octopus",
+  name: "Lizard",
   data() {
     return {};
   },
@@ -52,6 +55,6 @@ export default {
   top: 0;
   left: 0;
   width: 70%;
-  height: 110%;
+  height: 120%;
 }
 </style>
