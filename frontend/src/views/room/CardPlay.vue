5db87da139578f22d9831af783fce5b94e79a7b4
윤서완 <96missha@hanmail.net>
2021-05-06 13:55:11 +0900

    [#]fix : edit url

--- frontend/src/views/room/CardPlay.vue
+++ frontend/src/views/room/CardPlay.vue
@@ -2,29 +2,31 @@
   <!-- 질문 카드 게임 -->
   <div id="card-play">
     <h3> 질문 카드 게임 </h3>
+    <div id="test">test</div>
     <div id="rotate-div" class="chand"></div>
   </div>
 </template>
 
-
 <script>
-import jsFile from '@/assets/js/CMRotate.js'
-
+import CMRotate from '@/assets/js/CMRotate.js'
 export default {
   name: 'CardPlay',
   methods: {
 
   },
   created () {
-    var backgroundImages = ["../../assets/cards/sample/img1.jpg", "../../assets/cards/sample/img2.jpg", "../../assets/cards/sample/img3.jpg", "../../assets/cards/sample/img4.jpg", "../../assets/cards/sample/img5.jpg",
-                            "../../assets/cards/sample/img6.jpg", "../../assets/cards/sample/img7.jpg", "../../assets/cards/sample/img8.jpg", "../../assets/cards/sample/img9.jpg", "../../assets/cards/sample/img10.jpg",
-                            "../../assets/cards/sample/img11.jpg", "../../assets/cards/sample/img12.jpg", "../../assets/cards/sample/img13.jpg", "../../assets/cards/sample/img14.jpg", "../../assets/cards/sample/img15.jpg",
-                            "../../assets/cards/sample/img16.jpg", "../../assets/cards/sample/img17.jpg", "../../assets/cards/sample/img18.jpg"];
-    
-    jsFile.CMRotate.init('rotate-div', 200, 300, 100, 12, 600, backgroundImages, clickFn);
+    window.onload = function () {
 
-    function clickFn(no) {
-        alert('click no - ' + (no + 1));
+            var backgroundImages = ["cards/sample/img1.jpg", "images/img2.jpg", "images/img3.jpg", "images/img4.jpg", "images/img5.jpg",
+                                    "images/img6.jpg", "images/img7.jpg", "images/img8.jpg", "images/img9.jpg", "images/img10.jpg",
+                                    "images/img11.jpg", "images/img12.jpg", "images/img13.jpg", "images/img14.jpg", "images/img15.jpg",
+                                    "images/img16.jpg", "images/img17.jpg", "images/img18.jpg"];
+
+            CMRotate.init('rotate-div', 200, 300, 100, 12, 600, backgroundImages, clickFn);
+
+            function clickFn(no) {
+                alert('click no - ' + (no + 1));
+            }
     }
   }
 }
@@ -49,7 +51,7 @@ html, body {
 
 #rotate-div {
     overflow: hidden;
-    position: relative;
+    /* position: relative; */
     width:100%;
     height:100%;
 }
@@ -67,5 +69,10 @@ a:hover {
 }
 .chand:active {
     /* cursor: url(images/hand-h.cur), move; */
+}
+#test{
+    width: 300px;
+    height:300px;
+    background:url('C:/ssafy/LAST/s04p31a205/frontend/src/assets/cards/sample/img1.jpg');
 }
 </style>
