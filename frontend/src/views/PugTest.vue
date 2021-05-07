6a6bad39b32945e05214406b0a4da65ff4cb735d
이송영 <bulgen@naver.com>
2021-05-07 10:17:47 +0900

    [#] Refac: commit for clone

--- frontend/src/views/PugTest.vue
+++ frontend/src/views/PugTest.vue
@@ -73,7 +73,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 *, *:before, *:after {
 	border: 0;
 	box-sizing: border-box;
--- frontend/src/views/room/CardPlay.vue
+++ frontend/src/views/room/CardPlay.vue
@@ -3,7 +3,6 @@
   <div id="card-play">
     <h3> 질문 카드 게임 </h3>
     <img id="selected_card" src="@/assets/cards/card_front/black1.png" alt="asdasd">
-    <div class="my-3"></div>
     <div id="rotate-div" class="chand"></div>
     <!-- <img src="@/assets/cards/card_front/black1.png" alt=""> -->
   </div>
@@ -17,10 +16,11 @@ export default {
   methods: {
     clickCard (no) {
       // 매개인자 no를 넘겨받으면 해당 이미지로 변환
-      alert('click no -' + (no +1));
-      var selectedCard = document.getElementById("selected_card")
+        alert('click no -' + (no +1));
+        var selectedCard = document.getElementById("selected_card")
 
-      console.log(selectedCard)
+        selectedCard.style.backgroundImages = 'url("https://icebreaking205.s3.ap-northeast-2.amazonaws.com/front/black1.png")'
+        console.log(selectedCard)
     },
   },
   mounted () {
@@ -61,10 +61,10 @@ export default {
 #selected_card {
     /* background-image: url("../../assets/cards/card_front/img1.jpg"); */
     /* background-color: red; */
-    height: 60%;
-    width: 30%;
+    height: 50%;
+    width: 20%;
     position: absolute;
-    left: 35%;
+    left: 40%;
     /* display: none; */
 }
 
@@ -89,23 +89,6 @@ html, body {
     /* position: relative; */
     width:100%;
     height:100%;
-}
-
-#info-div {
-    /* width: 300px; */
-    /* height: 50px; */
-    position: absolute;
-    z-index: 12;
-    left: 50%;
-    top: 10px;
-    margin-left: -150px;
-    font-family: Helvetica, sans-serif;
-    font-size: 12px;
-    color: #333;
-    text-align: center;
-    line-height: 20px;
-    width: 100%;
-    height: 100%;
 }
 
 a {
