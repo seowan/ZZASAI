fd2723a6617f27604ea5b299cc6c07c4a10f9b77
이규용 <gyuyong290@gmail.com>
2021-05-14 13:43:12 +0900

    [#S04P31A205-202] feat: modify flip-card components import code

--- frontend/src/components/hall/Card.vue
+++ frontend/src/components/hall/Card.vue
@@ -1,24 +1,22 @@
 <template>
-  <div class="col-2 hall-card">
-    <div class="hall-card-inner">
-      <div class="hall-card-front">
-        <img  alt="Avatar" style="width:100px; height1300px;">
-      </div>
-      <div class="hall-card-back">
-        <h2 class="hall-card-text">질문카드</h2>
-      </div>
+  <div class="hall-card-inner">
+    <div class="hall-card-front">
+      <img alt="Avatar" style="width:100px; height1300px;" />
+    </div>
+    <div class="hall-card-back">
+      <h2 class="hall-card-text">질문카드</h2>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Card',
-}
+  name: "Card",
+};
 </script>
 
 <style scoped>
 img {
-  src: "@/assets/icons/card.png"
+  src: "@/assets/icons/card.png";
 }
 </style>
